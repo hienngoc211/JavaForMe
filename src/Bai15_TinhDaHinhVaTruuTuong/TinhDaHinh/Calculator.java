b8113476f5cb099e21a68ba9c0fa6474e1780372
Ngoc Hien <hienngoc211@gmail.com>
2023-11-20 09:07:44 +0700

    Commit Tinh Da hinh

---
+++ src/Bai15_TinhDaHinhVaTruuTuong/TinhDaHinh/Calculator.java
@@ -0,0 +1,22 @@
+package Bai15_TinhDaHinhVaTruuTuong.TinhDaHinh;
+
+public class Calculator {
+
+    public int tinhTong(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int tinhTong(int number1, int number2, int number3) {
+        return number1 + number2 + number3;
+    }
+
+    public double phepNhan(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public double phepNhan(float number1, float number2) {
+        return number1 * number2;
+    }
+
+
+}
